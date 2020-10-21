b8b58acf6d7455481ca7bebc742dfd8782652b1d
Artem Maksimikhin <uanov86@gmail.com>
2020-10-21 21:16:44 +0300

    Finished slider-gallery

--- js/script.js
+++ js/script.js
@@ -8,27 +8,23 @@ document.querySelector('.burger-menu-wrapper').onclick = function(){
     document.querySelector('.header__menu-arrow').classList.toggle('header__menu-arrow--active');
 
  }
- // Header-slider
-/* Индекс слайда по умолчанию */
-var slideIndex = 1;
+ /* Header-slider*/
+let slideIndex = 1;
 showSlides(slideIndex);
 autoSlider();
 document.getElementById('header-slider__btn-next').onclick = function () {
     plusSlide()
 };
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
 function plusSlide() {
     showSlides(slideIndex += 1);
 }
-/* Устанавливает текущий слайд */
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
-/* Основная функция сладера */
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("header-slider__item");
+    let i;
+    let slides = document.getElementsByClassName("header-slider__item");
     if (n > slides.length) {
         slideIndex = 1
     }
@@ -43,8 +39,8 @@ function showSlides(n) {
 }
 /*Автоматически выпадающие слайды сверху*/
 function autoSlider() {
-    var i;
-    var slides = document.getElementsByClassName("header-slider__item");
+    let i;
+    let slides = document.getElementsByClassName("header-slider__item");
     for (i = 0; i < slides.length; i++) {
 
         slides[i].classList.remove('header-slider__item--active');
@@ -58,4 +54,68 @@ function autoSlider() {
     setTimeout(autoSlider, 12000);
 };
 
- // Header-slider-end
+ /* Header-slider-end*/
+
+    /*slider-gallery*/
+let gallerySlideIndex = 1;
+let btnNext = document.getElementById('slider-gallery__next');
+let btnPrev = document.getElementById('slider-gallery__prev');
+btnNext.onclick = function () {
+    plusSlideGallery()
+};
+btnPrev.onclick = function () {
+    minusSlideGallery()
+};
+showSlidesGallery(gallerySlideIndex);
+
+
+function plusSlideGallery() {
+    showSlidesGallery(gallerySlideIndex += 1);
+}
+
+function minusSlideGallery() {
+    showSlidesGallery(gallerySlideIndex -= 1);
+}
+
+function currentSlideGallery(n) {
+    showSlidesGallery(gallerySlideIndex = n);
+}
+
+function showSlidesGallery(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider-gallery__item");
+    let slidesLength = slides.length
+
+    if (n > slides.length) {
+        gallerySlideIndex = 1
+    }
+    if (n < 1) {
+        gallerySlideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('slider-gallery__item--active');
+    }
+
+    slides[gallerySlideIndex - 1].classList.add('slider-gallery__item--active');
+    slides[gallerySlideIndex - 0].classList.add('slider-gallery__item--active');
+    slides[gallerySlideIndex + 1].classList.add('slider-gallery__item--active');
+    slides[gallerySlideIndex + 2].classList.add('slider-gallery__item--active');
+    slides[gallerySlideIndex + 3].classList.add('slider-gallery__item--active');
+    slides[gallerySlideIndex + 4].classList.add('slider-gallery__item--active');
+
+    if(slides[slidesLength-1].classList.contains('slider-gallery__item--active')){
+        btnNext.classList.add('active');
+
+    }
+    if(slides[slidesLength-7].classList.contains('slider-gallery__item--active')){
+        btnNext.classList.remove('active');
+    }
+
+    if(slides[0].classList.contains('slider-gallery__item--active')){
+        btnPrev.classList.add('active');
+
+    }
+    if(slides[6].classList.contains('slider-gallery__item--active')){
+        btnPrev.classList.remove('active');
+    }
+}
