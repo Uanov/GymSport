741acdd2ab0128bfb6126480071a7dddfec354e4
Artem Maksimikhin <uanov86@gmail.com>
2020-11-07 17:47:20 +0200

    Create page 'Contacts', finished section 'contacts-feedback'

--- js/script.js
+++ js/script.js
@@ -185,7 +185,7 @@ if (currentPage === '/index.html' || currentPage === '/' ) {
     }
 }
 
-if (currentPage === '/about.html' || currentPage === '/pricing.html') {
+if (currentPage === '/about.html' || currentPage === '/pricing.html' || currentPage === '/contacts.html') {
     document.querySelector('.burger-menu-wrapper').onclick = function () {
         document.querySelector('.burger-menu').classList.toggle('burger-menu--active');
         document.querySelector('.menu__list').classList.toggle('menu__list--active');
