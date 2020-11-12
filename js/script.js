707d34a3a6282e01b5c9722bf5f84cd5100964e9
Artem Maksimikhin <uanov86@gmail.com>
2020-11-12 15:41:13 +0200

    Finished page 'CLASSES-PILATES'

--- js/script.js
+++ js/script.js
@@ -251,6 +251,16 @@ if (currentPage === '/about.html' || currentPage === '/pricing.html' || currentP
             btnPrev.classList.remove('active');
         }
     }
+} else if (currentPage === '/classes_pilates.html'){
+    document.querySelector('.burger-menu-wrapper').onclick = function () {
+        document.querySelector('.burger-menu').classList.toggle('burger-menu--active');
+        document.querySelector('.menu__list').classList.toggle('menu__list--active');
+        document.querySelector('.header__body').classList.toggle('header__body--active');
+    }
+    document.querySelector('.menu__link--active').onclick = function () {
+        document.querySelector('.sub-menu__list').classList.toggle('sub-menu__list--active');
+
+    }
 }
 
 
