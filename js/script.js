b00d1739a7b8d3f47405a3b6cfb34ef2047948d8
Artem Maksimikhin <uanov86@gmail.com>
2020-11-16 12:45:21 +0200

    Finished page BLOG

--- js/script.js
+++ js/script.js
@@ -185,7 +185,7 @@ if (currentPage === '/index.html' || currentPage === '/' ) {
     }
 }
 
-if (currentPage === '/about.html' || currentPage === '/pricing.html' || currentPage === '/contacts.html'|| currentPage === '/classes.html') {
+if (currentPage === '/about.html' || currentPage === '/pricing.html' || currentPage === '/trainers.html' || currentPage === '/contacts.html'|| currentPage === '/classes.html') {
     document.querySelector('.burger-menu-wrapper').onclick = function () {
         document.querySelector('.burger-menu').classList.toggle('burger-menu--active');
         document.querySelector('.menu__list').classList.toggle('menu__list--active');
@@ -348,10 +348,73 @@ if (currentPage === '/about.html' || currentPage === '/pricing.html' || currentP
         dots[blogSlideIndex - 1].className += " blog-slider-dots__item--active";
         setTimeout(autoSlider, 5000);
     }
+    let slideIndexTrainers = 1;
+    let btnNextTrainers = document.getElementById('slider-trainers__next');
+    let btnPrevTrainers = document.getElementById('slider-trainers__prev');
+    btnNextTrainers.onclick = function () {
+        plusSlideTrainers()
+    };
+    btnPrevTrainers.onclick = function () {
+        minusSlideTrainers()
+    };
+    showSlidesTrainers(slideIndexTrainers);
 
 
+    function plusSlideTrainers() {
+        showSlidesTrainers(slideIndexTrainers += 1);
+    }
 
+    function minusSlideTrainers() {
+        showSlidesTrainers(slideIndexTrainers -= 1);
+    }
+
+    function showSlidesTrainers(n) {
+        let i;
+        let slides = document.getElementsByClassName("slider-trainers__item");
+        let slidesLength = slides.length
+
+        if (n > slides.length) {
+            slideIndexTrainers = 1
+        }
+        if (n < 1) {
+            slideIndexTrainers = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].classList.remove('slider-trainers__item--active');
+        }
+
+        slides[slideIndexTrainers - 1].classList.add('slider-trainers__item--active');
+        slides[slideIndexTrainers - 0].classList.add('slider-trainers__item--active');
+
+
+        if (slides[slidesLength - 1].classList.contains('slider-trainers__item--active')) {
+            btnNextTrainers.classList.add('slider-trainers__btn--active');
+
+        }
+        if (slides[slidesLength - 3].classList.contains('slider-trainers__item--active')) {
+            btnNextTrainers.classList.remove('slider-trainers__btn--active');
+        }
+
+        if (slides[0].classList.contains('slider-trainers__item--active')) {
+            btnPrevTrainers.classList.add('slider-trainers__btn--active');
+
+        }
+        if (slides[2].classList.contains('slider-trainers__item--active')) {
+            btnPrevTrainers.classList.remove('slider-trainers__btn--active');
+        }
+
+        let trainersBorder = document.getElementsByClassName("slider-trainers__border");
+        for (i = 0; i < 5; i++) {
+            if (slides[i].classList.contains('slider-trainers__item--active') && slides[i + 1].classList.contains('slider-trainers__item--active')) {
+                trainersBorder[i].classList.add('slider-trainers__border--active')
+            } else {
+                trainersBorder[i].classList.remove('slider-trainers__border--active')
+            }
+        }
+    }
 }
+
+
 
 
 
