bf33e5e7bc1c3fd0ba5df130d33893abbbd458ae
Artem Maksimikhin <uanov86@gmail.com>
2020-11-18 16:20:37 +0200

    Bugs fixed at JS, and footer links

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,16 @@
-  document.querySelector('.burger-menu-wrapper').onclick = function () {
-        document.querySelector('.burger-menu').classList.toggle('burger-menu--active');
-        document.querySelector('.menu__list').classList.toggle('menu__list--active');
-        document.querySelector('.header__body').classList.toggle('header__body--active');
-        document.querySelector('.header-slider').classList.toggle('header-slider--hidden');
+let headerSlider = document.querySelector('.header-slider');
 
+document.querySelector('.burger-menu-wrapper').onclick = function () {
+    document.querySelector('.burger-menu').classList.toggle('burger-menu--active');
+    document.querySelector('.menu__list').classList.toggle('menu__list--active');
+    document.querySelector('.header__body').classList.toggle('header__body--active');
+    if (headerSlider) {
+        headerSlider.classList.toggle('header-slider--hidden');
     }
-    document.querySelector('.menu__link--active').onclick = function () {
-        document.querySelector('.sub-menu__list').classList.toggle('sub-menu__list--active');
-        document.querySelector('.header-slider').classList.toggle('header__menu-arrow--active');
 
-    }
+}
+document.querySelector('.menu__link--active').onclick = function () {
+    document.querySelector('.sub-menu__list').classList.toggle('sub-menu__list--active');
+    document.querySelector('.header-slider').classList.toggle('header__menu-arrow--active');
+
+}
